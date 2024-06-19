0bd7430035707f6aec720dc35e18154c774adc55
Chaitanyabeeraka11 <chaitanyabeeraka@gmail.com>
2024-06-19 11:46:23 +0530

    some error rectified

--- Day_2/src/scanner_function.java
+++ Day_2/src/scanner_function.java
@@ -1,9 +1,10 @@
 import java.util.*;
 public class scanner_function {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("enter your name :");
-        String name = input.next();
-        System.out.println("your name is : "+ name);
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("enter your name :");
+            String name = sc.next();
+            System.out.println("your name is : "+ name);
+        }
     }
 }
