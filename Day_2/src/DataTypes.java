54aa22d8c83815e329df2ac01ec1be4404b102ff
Chaitanyabeeraka11 <chaitanyabeeraka@gmail.com>
2024-06-19 11:41:04 +0530

    two files added

---
+++ Day_2/src/DataTypes.java
@@ -0,0 +1,17 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        int rollnumber = 1;
+        char initial = 'B';
+        String name = "chaitanya";
+        float percentage = 85.5f;
+        double attendence = 500.55;
+        short number = 12343;
+        Boolean present = true; 
+        System.out.println("Name : "+initial +" "+ name);
+        System.out.println("rollnumber : "+ rollnumber + " phone : "+number);
+        System.out.println("present :"+present + " " + "attendence :"+ attendence);
+        System.out.println("percentage : "+percentage);
+
+    }
+    
+}
---
+++ Day_2/src/scanner_function.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class scanner_function {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter your name :");
+        String name = input.next();
+        System.out.println("your name is : "+ name);
+    }
+}
