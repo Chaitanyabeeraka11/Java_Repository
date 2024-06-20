29226b32c5301655382b86e310cf66dda826f13a
Chaitanyabeeraka11 <chaitanyabeeraka@gmail.com>
2024-06-20 12:06:08 +0530

    switch_statements code added

--- Day_2/src/switch_case.java
+++ Day_2/src/switch_case.java
@@ -3,34 +3,22 @@ import java.util.Scanner;
 
 public class switch_case {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("enter day name : ");
-        String day = sc.next();
-        switch(day){
-            case "sunday":
-                System.out.println("day1");
-                break;
-            case "monday":
-                System.out.println("day2");
-                break;
-            case "tuesday":
-                System.out.println("day3");
-                break;
-            case "wednesday":
-                System.out.println("day4");
-                break;
-            case "thursday":
-                System.out.println("day5");
-                break;
-            case "friday":
-                System.out.println("day6");
-                break;
-            case "saturday":
-                System.out.println("day7");
-                break;
-            default:
-                System.out.println("invalid day");
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("enter day name : ");
+            int daynum = 0;
+            String day = sc.next();
+            switch(day){
+                case "sunday" ->  daynum = 1;
+                case "monday" -> daynum = 2;
+                case "tuesday" -> daynum = 3;
+                case "wednesday" -> System.out.println("day4");
+                case "thursday" -> System.out.println("day5");
+                case "friday" -> System.out.println("day6");
+                case "saturday" -> System.out.println("day7");
+                default -> System.out.println("invalid day");
 
+            }
+            System.out.println("day number : "+ daynum);
         }
     }
 }
