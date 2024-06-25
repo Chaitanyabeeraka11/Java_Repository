2bfb2ea0cf1a995104b18fd932b82ef3557d927c
Chaitanyabeeraka11 <chaitanyabeeraka@gmail.com>
2024-06-25 12:00:01 +0530

    Objects learned

---
+++ Day_2/src/Objects.java
@@ -0,0 +1,10 @@
+public class Objects {
+    static int a = 5;
+    public static void main(String[] args) {
+        
+        Objects obj = new Objects();
+        System.out.println(a);
+        obj.a = 6;
+        System.out.println(obj.a);
+    }
+}
